86490c15925d7f895dc441daacd0c4924a363edc
jcohen42 <42759062+jcohen42@users.noreply.github.com>
2021-12-15 15:42:50 -0700

    IDE improvements
    Made window larger
    Improved visuals
    Added syntax highlighting and line numbers with ScintillaNET
    Bug fixes

---
+++ EngramIDE/EngramIDE/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace EngramIDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.panelOutputBG = new System.Windows.Forms.Panel();
+            this.editorTextBox = new ScintillaNET.Scintilla();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.panelOutputBG.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(195)))), ((int)(((byte)(230)))));
+            this.loadButton.FlatAppearance.BorderSize = 0;
+            this.loadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.loadButton.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadButton.Location = new System.Drawing.Point(17, 56);
+            this.loadButton.Margin = new System.Windows.Forms.Padding(8);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(199, 40);
+            this.loadButton.TabIndex = 1;
+            this.loadButton.Text = "Load from Computer";
+            this.loadButton.UseVisualStyleBackColor = false;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // runButton
+            // 
+            this.runButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(166)))), ((int)(((byte)(202)))));
+            this.runButton.FlatAppearance.BorderSize = 0;
+            this.runButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.runButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.runButton.Location = new System.Drawing.Point(657, 56);
+            this.runButton.Margin = new System.Windows.Forms.Padding(8);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(108, 40);
+            this.runButton.TabIndex = 2;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = false;
+            this.runButton.Click += new System.EventHandler(this.runButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(195)))), ((int)(((byte)(230)))));
+            this.saveButton.FlatAppearance.BorderSize = 0;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.Location = new System.Drawing.Point(442, 56);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(8);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(199, 40);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save to Computer";
+            this.saveButton.UseVisualStyleBackColor = false;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.AcceptsTab = true;
+            this.outputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(137)))), ((int)(((byte)(184)))));
+            this.outputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputTextBox.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.outputTextBox.Location = new System.Drawing.Point(8, 0);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(8);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(598, 535);
+            this.outputTextBox.TabIndex = 4;
+            this.outputTextBox.Text = "";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(190)))), ((int)(((byte)(221)))));
+            this.inputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.inputTextBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(657, 665);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(8);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(455, 40);
+            this.inputTextBox.TabIndex = 5;
+            this.inputTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputTextBox_KeyDown);
+            // 
+            // enterButton
+            // 
+            this.enterButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(166)))), ((int)(((byte)(202)))));
+            this.enterButton.FlatAppearance.BorderSize = 0;
+            this.enterButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.enterButton.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterButton.Location = new System.Drawing.Point(1128, 663);
+            this.enterButton.Margin = new System.Windows.Forms.Padding(8);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(135, 40);
+            this.enterButton.TabIndex = 6;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = false;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(166)))), ((int)(((byte)(202)))));
+            this.stopButton.Enabled = false;
+            this.stopButton.FlatAppearance.BorderSize = 0;
+            this.stopButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.stopButton.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.stopButton.Location = new System.Drawing.Point(1155, 56);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(8);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(108, 40);
+            this.stopButton.TabIndex = 8;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(205)))), ((int)(((byte)(214)))));
+            this.labelTitle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.labelTitle.Location = new System.Drawing.Point(0, 0);
+            this.labelTitle.Margin = new System.Windows.Forms.Padding(8);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(1280, 40);
+            this.labelTitle.TabIndex = 10;
+            this.labelTitle.Text = "Engram IDE";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.labelTitle_MouseDown);
+            // 
+            // panelOutputBG
+            // 
+            this.panelOutputBG.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(137)))), ((int)(((byte)(184)))));
+            this.panelOutputBG.Controls.Add(this.outputTextBox);
+            this.panelOutputBG.Location = new System.Drawing.Point(657, 112);
+            this.panelOutputBG.Margin = new System.Windows.Forms.Padding(8);
+            this.panelOutputBG.Name = "panelOutputBG";
+            this.panelOutputBG.Size = new System.Drawing.Size(606, 535);
+            this.panelOutputBG.TabIndex = 12;
+            // 
+            // editorTextBox
+            // 
+            this.editorTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.editorTextBox.Location = new System.Drawing.Point(17, 112);
+            this.editorTextBox.Margin = new System.Windows.Forms.Padding(8);
+            this.editorTextBox.Name = "editorTextBox";
+            this.editorTextBox.Size = new System.Drawing.Size(624, 591);
+            this.editorTextBox.TabIndex = 13;
+            this.editorTextBox.TabWidth = 6;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(195)))), ((int)(((byte)(194)))));
+            this.buttonExit.FlatAppearance.BorderSize = 0;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(1240, 0);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(40, 40);
+            this.buttonExit.TabIndex = 14;
+            this.buttonExit.Text = "X";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(230)))), ((int)(((byte)(239)))));
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.editorTextBox);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.panelOutputBG);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Engram IDE";
+            this.panelOutputBG.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Panel panelOutputBG;
+        private ScintillaNET.Scintilla editorTextBox;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ EngramIDE/EngramIDE/Form1.cs
@@ -0,0 +1,275 @@
+﻿/*
+ * Jake Cohen
+ * Arizona State University
+ * Engram IDE
+ */
+
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using ScintillaNET;
+
+namespace EngramIDE
+{
+    public partial class Form1 : Form
+    {
+        Process compiler;
+        StreamWriter myStreamWriter;
+        string inputBuffer = "";
+
+        int OUTPUT = 0;
+        int INPUT = 1;
+        int MAXIO = 2048;
+
+        //Start the Engram compiler as a Process. Display output and gather output
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = "compiler.exe";
+            info.Arguments = "engram.txt -showInputFlag";
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardInput = true;
+            info.CreateNoWindow = true;
+
+            compiler = Process.Start(info);
+            myStreamWriter = compiler.StandardInput;
+
+            int ioCount = 0;
+            while (!compiler.HasExited)
+            {
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                if (ioCount >= MAXIO)
+                {
+                    worker.ReportProgress(OUTPUT, "The program exited prematurely because an infinite loop was encountered.");
+                    myStreamWriter.Close();
+                    compiler.Close();
+                    return;
+                }
+
+                string output = compiler.StandardOutput.ReadLine();
+                if (output == "!@#INPUT#@!")
+                {
+                    while (inputBuffer == "") { if (worker.CancellationPending) return; }
+                    worker.ReportProgress(INPUT);
+                }
+                else
+                {
+                    worker.ReportProgress(OUTPUT, output);
+                }
+
+                ioCount++;
+            }
+            worker.ReportProgress(OUTPUT, compiler.StandardOutput.ReadToEnd());
+
+            myStreamWriter.Close();
+            compiler.WaitForExit();
+            compiler.Close();
+        }
+
+        //Append output to outputTextBox and gather user input if necessary
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage == OUTPUT)
+            {
+                outputTextBox.Text += (e.UserState as String) + "\n";
+            }
+            else
+            {
+                myStreamWriter.WriteLine(inputBuffer);
+                inputBuffer = "";
+            }
+        }
+
+        //Once the program has terminated, we can re-enable the Run button and disable the Stop button
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            stopButton.Enabled = false;
+            runButton.Enabled = true;
+            runButton.Text = "Run";
+        }
+
+        //This method was adapted from: https://github.com/jacobslusser/ScintillaNET/wiki/Automatic-Syntax-Highlighting
+        //Set the color and styling for the tokens of the Engram language
+        public void setEditorHighlighting()
+        {
+            //Configure the default style properties
+            editorTextBox.StyleResetDefault();
+            editorTextBox.Styles[Style.Default].BackColor = Color.FromArgb(1, 184, 190, 221);
+            editorTextBox.Styles[Style.Default].Font = "Segoe UI";
+            editorTextBox.Styles[Style.Default].Size = 14;
+            editorTextBox.StyleClearAll();
+            
+            //Configure the C++ lexer styles
+            //I use C++ because Engram and C++ and somewhat lixically ismilar
+            editorTextBox.Styles[Style.Cpp.Default].ForeColor = Color.Silver;
+            editorTextBox.Styles[Style.Cpp.Number].ForeColor = Color.FromArgb(0, 128, 0); // Green - numbers
+            editorTextBox.Styles[Style.Cpp.Word].ForeColor = Color.Blue; // Primary keywords and identifiers
+            editorTextBox.Styles[Style.Cpp.Word2].ForeColor = Color.Purple; // Secondary keywords and identifiers
+            editorTextBox.Styles[Style.Cpp.String].ForeColor = Color.FromArgb(163, 21, 21); // Red
+            editorTextBox.Styles[Style.Cpp.StringEol].BackColor = Color.Pink;
+            editorTextBox.Lexer = Lexer.Cpp;
+
+            //Set the keywords
+            //-Primary keywords and identifiers
+            editorTextBox.SetKeywords(0, "Set Call While while End Begin If if Else with argument Return program");
+            //-Secondary keywords and identifiers
+            editorTextBox.SetKeywords(1, "equal to not is less greater than plus minus times divided by modulo Output Input set return");
+        }
+
+        //Create a rounded control with height and width
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+
+        //Prepare the form for displaying
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Set line number margins
+            editorTextBox.Margins[0].Width = 24;
+            editorTextBox.Margins[1].Width = 0;
+
+            //Prepare for syntax highlighting
+            setEditorHighlighting();
+
+            //Create rounded corners for each text field, button, and the window itself
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            outputTextBox.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, outputTextBox.Width, outputTextBox.Height, 20, 20));
+            panelOutputBG.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, panelOutputBG.Width, panelOutputBG.Height, 20, 20));
+            editorTextBox.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, editorTextBox.Width, editorTextBox.Height, 20, 20));
+            runButton.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, runButton.Width, runButton.Height, 10, 10));
+            stopButton.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, stopButton.Width, stopButton.Height, 10, 10));
+            saveButton.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, saveButton.Width, saveButton.Height, 10, 10));
+            loadButton.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, loadButton.Width, loadButton.Height, 10, 10));
+            enterButton.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, enterButton.Width, enterButton.Height, 10, 10));
+
+            //Add hint text to the input text box
+            SendMessage(inputTextBox.Handle, 0x1501, 1, "Type a number...");
+
+            //Set the focus to the editor
+            this.ActiveControl = editorTextBox;
+            editorTextBox.Focus();
+        }
+
+        //Drag form - adapter from: https://rjcodeadvance.com/crear-formulario-de-bordes-redondeados-suaves-c-winforms/
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void labelTitle_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        //This extern method allows us to display hint text on the inputTextBox
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        //Run the Engram compiler as a Background Worker
+        private void runButton_Click(object sender, EventArgs e)
+        {
+            //Prepare for running the program
+            outputTextBox.Text = "";
+            runButton.Text = "Running...";
+            runButton.Enabled = false;
+            stopButton.Enabled = true;
+
+            //Save the program to a file
+            FileInfo myFile = new FileInfo("engram.txt");
+            myFile.Attributes &= ~FileAttributes.Hidden;
+            File.WriteAllText("engram.txt", editorTextBox.Text);
+            myFile.Attributes |= FileAttributes.Hidden;
+
+            //Empty the input buffer
+            inputBuffer = "";
+
+            //Use the background worker to run the program
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        //Checks if the user types Enter in the inputTextBox, and calls the Enter Button method if so
+        private void inputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            //If the enter key is pressed, act as if the enter button was pressed
+            if (e.KeyCode == Keys.Enter)
+            {
+                enterButton_Click(sender, e);
+
+                e.SuppressKeyPress = true; //Stops the 'ding' noise
+            }
+        }
+
+        //Store the input text box value to the inputBuffer
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            inputBuffer = inputTextBox.Text;
+            inputTextBox.Text = "";
+        }
+
+        //Stop the running program
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+        //Loads a .txt file using File Explorer and displays it in the editor
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            //Prompt the user to open a file
+            openFileDialog.Filter = "Text|*.txt";
+            openFileDialog.ShowDialog();
+
+            //Open the file
+            string fileName = openFileDialog.FileName;
+
+            try
+            {
+                //Pass the lines to the editor text box
+                string[] lines = File.ReadAllLines(fileName);
+                editorTextBox.Text = "";
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    editorTextBox.Text += lines[i] + "\n";
+                }
+            } catch (FileNotFoundException err) { }
+        }
+
+        //Saves the text written in the editor to engram.txt by default
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            //Save the editor text box to the computer
+            saveFileDialog.Filter = "Text|*.txt";
+            saveFileDialog.Title = "Save your program";
+            saveFileDialog.FileName = "engram";
+            saveFileDialog.ShowDialog();
+
+            File.WriteAllLines(saveFileDialog.FileName, editorTextBox.Text.Split('\n'));
+        }
+
+        //Exits the IDE application
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
